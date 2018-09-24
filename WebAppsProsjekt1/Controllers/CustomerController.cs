8cdf752691e5b83df638e509adf7d6adf06299d0
unknown <viktor.v.johansen@hotmail.com>
2018-09-24 20:35:36 +0200

    Not working DB feelsbadman with gun

---
+++ WebAppsProsjekt1/Controllers/CustomerController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppsProsjekt1.Models;
+
+namespace WebAppsProsjekt1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer/NewCustomer
+        [HttpPost]
+        public ActionResult NewCustomer(Models.Customer inCustomer)
+        {
+            //var newCustomer = new Customer{ Name = "Shrek" };
+            //return View(newCustomer);
+            using (var db = new Models.DB())
+            {
+                try
+                {
+                    db.Customer.Add(inCustomer);
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            return RedirectToAction("List");
+        }
+    }
+
+    //public ActionResult CustomerList()
+    //{
+    //    using (var db = new Models.DB())
+    //    {
+    //        List<Models.Customer> allCustomers = db.Customer.ToList();
+    //        return View(allCustomers);
+    //    }
+    //}
+}
---
+++ WebAppsProsjekt1/Models/DB.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace WebAppsProsjekt1.Models
+{
+    public class DB : DbContext
+    {
+        public DB()
+            : base("name=Customer")
+        {
+            Database.CreateIfNotExists();
+        }
+        public DbSet<Customer> Customer { get; set; }
+        
+    }
+}
