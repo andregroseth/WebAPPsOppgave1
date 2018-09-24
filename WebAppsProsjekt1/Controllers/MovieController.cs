d8ecc5bef573e3d227aee7dfe4a05db5aadf0bbd
Carlo <s315613@stud.hioa.no>
2018-09-24 16:21:55 +0200

    Movie branch

---
+++ WebAppsProsjekt1/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppsProsjekt1.Models;
+
+namespace WebAppsProsjekt1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Film
+        public ActionResult MovieView1()
+        {
+            var movie = new Movie() { Name = "Shrek" };
+            return View(movie);
+        }
+    }
+}
