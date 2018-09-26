16e9757f7a87f37eb5a0853184b52c182f0e44bc
andregroseth <andregroseth@hotmail.com>
2018-09-26 14:29:51 +0200

    testing shit

--- WebAppsProsjekt1/App_Start/RouteConfig.cs
+++ WebAppsProsjekt1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebAppsProsjekt1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Kunde", action = "Liste", id = UrlParameter.Optional }
             );
         }
     }
