654b9412c638fa80e9f737df65e1b95036434e10
Carlo <s315613@stud.hioa.no>
2018-10-04 14:37:01 +0200

    DBInit har nå filmer

--- WebAppsProsjekt1/Models/DbInit.cs
+++ WebAppsProsjekt1/Models/DbInit.cs
@@ -10,7 +10,7 @@ namespace WebAppsProsjekt1.Models
     {
         protected override void Seed(DB context)
         {
-            List<Movie> testMovieList = new List<Movie>
+            List<Movie> MovieList = new List<Movie>
                 {
                     new Movie
                     {
@@ -34,7 +34,150 @@ namespace WebAppsProsjekt1.Models
                         Category = "Sci-Fi",
                         Cost = 90,
                         ImagePath = "/Content/images/movie/antmanImage.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Back To The Future",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/backtothefuture.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Black Panther",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/blackpanther.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Deadpool 2",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/deadpool2.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Gladiator",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/gladiatorImage.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Hitch Hikers Guide",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/hitchhikersguide.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Hitman",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/hitmanImage.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "iBoy",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/iboy.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Inception",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/inception.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Infinity War",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/infinitywar.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Justice League",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/justiceleague.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Kong Skull Island",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/kongImage.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Limitless",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/limitless.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Ready Player One",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/readyplayerone.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Stars Wars Solo",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/solo.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Amazing Spider Man",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/theamazingspidermanImage.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Tron",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/tron.jpg"
+                    },
+
+                    new Movie
+                    {
+                        Title = "Tron: Legacy",
+                        Category = "Sci-Fi",
+                        Cost = 90,
+                        ImagePath = "/Content/images/movie/tronlegacy.jpg"
                     }
+
+
+                };
+
+            List<Movie> UserList = new List<Movie>
+                {
+                    
                 };
 
             var testUser = new User
@@ -77,7 +220,7 @@ namespace WebAppsProsjekt1.Models
             context.User.Add(testUser);
 
             // Legger inn init-filmer til databasen
-            foreach (var movie in testMovieList)
+            foreach (var movie in MovieList)
             {
                 context.Movie.Add(movie);
             }
