bec7c7a4bc8e88e11d2d7bef1882dad51554958f
JeremiahUy <jery_3000@hotmail.com>
2018-10-23 12:30:26 +0200

    fikset lagdeling, lagdeling ferdig

--- Model/VMUser.cs
+++ Model/VMUser.cs
@@ -5,7 +5,6 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
 namespace WebAppsProsjekt1.Model
 {
