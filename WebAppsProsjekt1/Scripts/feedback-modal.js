89731d837b38075ee375b5bd4c726a4a6b5b3185
Vartdalen <viktor.v.johansen@hotmail.com>
2018-10-26 16:57:49 +0200

    improved modal

--- WebAppsProsjekt1/Scripts/feedback-modal.js
+++ WebAppsProsjekt1/Scripts/feedback-modal.js
@@ -2,5 +2,8 @@
 	$("#feedback-modal").find('p').html(inputString);
 	$("#feedback-modal").css('background-color', color);
 	$("#feedback-modal").slideToggle().slideDown();
+	$("#feedback-modal-close").on("click", function () {
+		$("#feedback-modal").remove();
+	});
 	$("#feedback-modal").delay(delay).slideToggle().slideUp();
 }
